0649965ad9f8cd4d1498e24ce9512505ed4fe83a
Berserker1523 <berserker1523@hotmail.com>
2019-09-15 10:59:14 -0500

    Modificada la página de inicio del backend

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'EvUniandos.18 back-end'});
+  res.render('../front/build/index.html', {title: 'EvUniandos.18'});
 });
 
 module.exports = router;
