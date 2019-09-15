95984be7046de662426ad85243a5cc462e55ed95
Berserker1523 <berserker1523@hotmail.com>
2019-09-15 11:16:28 -0500

    prueba heroku #1231231

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'EvUniandos.18'});
+  res.render('../front/public/index', {title: 'EvUniandos.18'});
 });
 
 module.exports = router;
