274e688bb5260bfad3a441113c34f78f0bc41731
Berserker1523 <berserker1523@hotmail.com>
2019-09-15 10:40:50 -0500

    Proc file

--- front/src/Event.js
+++ front/src/Event.js
@@ -1,5 +1,30 @@
 import React from "react";
 
+function PutUserEventRelationship(){
+    fetch("/users", {
+      method: 'PUT', // or 'PUT'
+      body: JSON.stringify(
+      {
+        name: this.state.nameUserInput,
+        mail: this.state.emailUserInput,
+        password: this.state.passwordUserInput,
+        birthdate: this.state.birthdateUserInput,
+        schedule:[]
+      }
+      ), // data can be `string` or {object}!
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(response =>
+      console.log('Success:', response),
+      console.log("En SignUp: " + this.state.nameUserInput + " " + this.state.emailUserInput + " " +
+       this.state.passwordUserInput + " " + this.state.birthdateUserInput),
+      this.props.changeState(this.state.nameUserInput, this.state.emailUserInput,
+        this.state.passwordUserInput, this.state.birthdateUserInput),
+    );
+};
 
 const Event = (props) =>{
   return(
